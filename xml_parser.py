dcf56724c7331bb6cdd6dc75dfb359241d8fc21b
Jason Willems <jwillems@twilio.com>
2016-11-06 21:28:08 -0800

    working version that pulls last 'n' articles

--- xml_parser.py
+++ xml_parser.py
@@ -1,20 +1,25 @@
-from   BeautifulSoup import BeautifulSoup
-from   lxml import etree
-import pdb
+from   dependencies.BeautifulSoup import BeautifulSoup
+import httplib
 import re
 import unicodedata
+import xml.etree.ElementTree as ET
+import pdb
 
-
-FEED_URL        = 'http://www.speedlighter.ca/feed/'
-FILTER_CATEGORY = 'Digital Darkroom'
+SITE_URL        = 'www.speedlighter.ca'
+FILTER_CATEGORY = 'Audible'
 TEXT_BLOCK      = '{http://purl.org/rss/1.0/modules/content/}encoded'
 MAX_PAGES       = 10
-audible_posts   = []
 
 
 def get_site_content(page=1):
   # Get full XML RSS Feed
-  tree = etree.parse(FEED_URL + '?paged={}'.format(page))
+  page_query = '?paged={}'.format(page) if page > 1 else '' # paged=1 issues HTTP redirect
+
+  conn = httplib.HTTPConnection(SITE_URL, 80)
+  conn.request('GET', '/feed/' + page_query)
+  response = conn.getresponse()
+  tree = ET.parse(response)
+  conn.close()
   return tree.getroot()
 
 def get_items_from_page(page):
@@ -29,13 +34,40 @@ def extract_date(date_time_str):
   except:
     return date_time_str
 
+def get_verbal_text_string(input_text):
+  # Remove unexpected formatting before text can be verbalized
+  try:    clean_text = unicodedata.normalize('NFKD', unicode(input_text))
+  except: clean_text = unicodedata.normalize('NFKD', input_text)
+
+  clean_text = remove_url_paths(clean_text)
+  clean_text = replace_unpronounceable_chars(clean_text)
+  clean_text = remove_unpronounceable_chars(clean_text)
+  return clean_text
+
+def remove_url_paths(input_text):
+  # Strip everything except base URL : https://www.speedlighter.ca/path/to/resource => www.speedlighter.ca
+  try:    return re.sub("(https?://|www.)(?P<url>[^\/]+)/?[^\s]+", "\\2", input_text)
+  except: return input_text
+
+def replace_unpronounceable_chars(input_string):
+  # Remove newlines and replace a few specific unicode characters
+  return input_string.replace('\n', ' ').replace(u"\u2018", "'").replace(u"\u2019", "'")
+
+def remove_unpronounceable_chars(input_string):
+  # Removes reamining unicode chacters : "hello \u2016 world" => "hello world"
+  try:
+    return input_string.encode('ascii','ignore')
+  except:
+    try:    return input_string.decode('unicode_escape').encode('ascii','ignore')
+    except: return input_string
+
 
 def get_page(page_num=1):
   xml_page = get_site_content(page_num)
   return get_items_from_page(xml_page)
 
 def get_posts_matching_category(all_posts, target_category):
-  # Extract articles with FILTER_CATEGORY tag
+  # Extract articles with target_category set
   matching_posts = []
 
   for post in all_posts:
@@ -47,35 +79,49 @@ def get_posts_matching_category(all_posts, target_category):
   return matching_posts
 
 
+def get_extracted_text(articles=1):
 
-for page_number in range(0, MAX_PAGES):
+  matches = []
+  for page_number in range(0, MAX_PAGES):
 
-  all_posts     = get_page(page_number)
-  audible_posts = get_posts_matching_category(all_posts, FILTER_CATEGORY)
+    all_posts     = get_page(page_number + 1)
+    audible_posts = get_posts_matching_category(all_posts, FILTER_CATEGORY)
 
-  if len(audible_posts) == 0:
-    continue
+    if len(audible_posts) == 0:
+      continue
+    if len(matches) >= articles:
+      break
 
-  
+    # Get content from audible posts
+    for target_post in audible_posts:
 
-  # Get content from audible posts
-  for target_post in audible_posts:
+      if len(matches) >= articles:
+        break
 
-    title_str   = target_post.find('title').text or ''
-    date_str    = extract_date(target_post.find('pubDate').text)
-    content_str = target_post.find(TEXT_BLOCK).text
 
-    s = BeautifulSoup(content_str, convertEntities=BeautifulSoup.HTML_ENTITIES)
-    safe_html = ''.join(s.findAll(text=True))
+      # Extract three desired pieces of information from XML
+      title_str   = target_post.find('title').text or 'Untitled Post'
+      date_str    = extract_date(target_post.find('pubDate').text) or 'Unknown Date'
+      content_str = target_post.find(TEXT_BLOCK).text or 'No Content'
 
-    safe_html = unicodedata.normalize('NFKD', safe_html)
+      # Extract text from within XML tags
+      s = BeautifulSoup(content_str, convertEntities=BeautifulSoup.HTML_ENTITIES)
+      safe_html = ''.join(s.findAll(text=True))
 
-    print '{} published on {}'.format(unicode(title_str), unicode(date_str))
-    print safe_html
+      safe_html = get_verbal_text_string(safe_html)
+      title_str = get_verbal_text_string(title_str)
 
-  break
+      # return first result found
+      matches.append({'content': safe_html,
+                      'title':   title_str,
+                      'date':    date_str})
 
-  print 'Sorry, there are no recent or archived posts currently available'
 
+  if len(matches) == 0:
+    return [{'content': 'Sorry, there are no recent or archived posts currently available. Please try again later',
+             'title':   '',
+             'date':    ''}]
+  else:
+    return matches
 
 
